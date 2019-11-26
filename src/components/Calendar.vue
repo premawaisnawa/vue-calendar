828261ff51b7be9747797ba7b5d4d7c8affd8b68
Kade Prema Waisnawa <premawaisnawa@gmail.com>
2019-11-26 20:15:44 +0800

    update gitignore

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -47,7 +47,30 @@
               <v-text-field v-model="details" type="text" label="detail"></v-text-field>
               <v-text-field v-model="start" type="date" label="start (required)"></v-text-field>
               <v-text-field v-model="end" type="date" label="end (required)"></v-text-field>
-              <v-text-field v-model="color" type="color" label="color (click to open color menu)"></v-text-field>
+              <!-- <v-text-field v-model="color" type="color" label="color (click to open color menu)"></v-text-field> -->
+              <v-container>
+                <v-row justify="space-around">
+                  <!-- <v-switch v-model="showSwatches" class="mx-2" label="Show Swatches"></v-switch>
+                  <v-switch v-model="hideInputs" class="mx-2" label="Hide Inputs"></v-switch>
+                  <v-switch v-model="hideCanvas" class="mx-2" label="Hide Canvas"></v-switch>
+                  <v-switch v-model="hideModeSwitch" class="mx-2" label="Hide Mode Switch"></v-switch>
+
+                  <v-col class="mx-2" cols="3">
+                    <v-select v-model="mode" :items="modes" label="Mode"></v-select>
+                  </v-col>-->
+                </v-row>
+              </v-container>
+
+              <v-color-picker
+                v-model="color"
+                :hide-canvas="hideCanvas"
+                :hide-inputs="hideInputs"
+                :hide-mode-switch="hideModeSwitch"
+                :mode.sync="mode"
+                :show-swatches="showSwatches"
+                class="mx-auto"
+              ></v-color-picker>
+
               <v-btn
                 type="submit"
                 color="primary"
@@ -140,6 +163,12 @@ export default {
     selectedOpen: false,
     events: [],
     dialog: false,
+    hideCanvas: false,
+    hideInputs: false,
+    hideModeSwitch: false,
+    mode: "hexa",
+    modes: ["rgba", "hsla", "hexa"],
+    showSwatches: false,
     dialogDate: false
   }),
   computed: {
